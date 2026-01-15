0c815661b24b474589eca3dbde9dca3d3bd3c41c
237 <45787687+from237@users.noreply.github.com>
2026-01-15 10:04:39 +0900

    Data Update: 2026-01-15 10:04:39

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # ==========================================
 # [설정] 수집 날짜 (자동 분할 저장됨)
 # ==========================================
-START_DATE = date(2026, 1, 1)
-END_DATE = date(2026, 1, 13)
+START_DATE = date(2026, 1, 14)
+END_DATE = date(2026, 1, 14)
 
 WEIGHT_FOLDER = "weights"
 MASTER_WEIGHT_FILE = "weight_2022_2025.csv"
