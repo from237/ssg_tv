0b95fa2ebf29128d37e6f15829e6144623c9e808
Kim Jongbaek <smile4menow@naver.com>
2026-06-01 07:41:28 +0900

    Data Update: 2026-06-01 07:41:28

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # ==========================================
 # [설정] 수집 날짜 (자동 분할 저장됨)
 # ==========================================
-START_DATE = date(2026, 5, 26)
-END_DATE = date(2026, 5, 27)
+START_DATE = date(2026, 5, 28)
+END_DATE = date(2026, 5, 31)
 
 WEIGHT_FOLDER = "weights"
 MASTER_WEIGHT_FILE = "weight_2022_2025.csv"
